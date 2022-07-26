58774f46313fe5a3934d659665ea2b64802ec0b0
ElShaimaa <Elshaimaa.hassan@gmail.com>
2022-07-27 00:10:28 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { IntroComponent } from './intro/intro.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { InfoComponent } from './info/info.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    IntroComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactMeComponent,
+    InfoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
